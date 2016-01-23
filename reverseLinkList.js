752012adc7074a319e481078cf22ba288e8e8c89
Tina Lai <etchaway@gmail.com>
2016-01-22 19:37:54 -0800

    implement reverse Linkedlist

---
+++ reverseLinkList.js
@@ -0,0 +1,14 @@
+const reverseLinklist = (linklist) => {
+  let node = linklist;
+  let prev = null;
+
+  while(node) {
+    let temp = node.next;
+    node.next = prev;
+    prev = node;
+    node = temp;
+  }
+  return prev;
+};
+
+/* You can also implement it using a recursive function. However then the time complexity will not be constant anymore.*/
