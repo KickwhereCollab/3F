183c7184337fd7e04c6694d8b8481cf4bf9eb9a4
Tina Lai <etchaway@gmail.com>
2016-01-25 16:16:11 -0800

    changes to pseudoclassical style

--- stack.js
+++ stack.js
@@ -1,27 +1,38 @@
-const Stack = () => {
-  let storage = [];
-  let max = [];
-
-  this.push = (int) => {
-    if (max.length === 0) {
-      max.push(int);
-    } else {
-      let last = max.length - 1;
-      if(max[last] > int) {
-        max.push(last);
-      } else {
-        max.push(int);
-      }
-    }
-    storage.push(int);
-  };
-
-  this.pop = () => {
-    max.pop();
-    return storage.pop();
-  };
-
-  this.getMax = () => {
-    return max[max.length-1];
-  };
+var Stack = function(){
+  this.storage =[];
+  this.max = [];
 };
+
+Stack.prototype.push = function(int){
+  this.storage.push(int);
+  var largest = this.max[this.max.length-1];
+  if(this.max.length === 0 || largest < int) {
+     this.max.push(int);
+  }
+};
+
+Stack.prototype.pop = function() {
+  var popped = this.storage.pop();
+  if(popped === this.getMax()) {
+    this.max.pop();
+  }
+  return popped;
+};
+
+Stack.prototype.getMax = function() {
+  return this.max[this.max.length-1];
+};
+
+// Helper function to check what storage looks like
+Stack.prototype.getStorage = function () {
+  return this.storage;
+};
+
+// Helper function to check what max storage looks like
+Stack.prototype.maxStorage = function() {
+  return this.max;
+};
+
+/*
+  This implementation assumes that all input integers are unique. Limitations: If there are repeated inputs this implementation will not work.
+*/
