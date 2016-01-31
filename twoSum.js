a0e4ebfef38f750be8c6e4218a5dd0a04e732e9f
Tina Lai <etchaway@gmail.com>
2016-01-31 09:36:24 -0800

    adds two sum result

--- twoSum.js
+++ twoSum.js
@@ -5,3 +5,14 @@ The function twoSum should return indices of the two numbers such that they add 
 
 You may assume that each input would have exactly one solution.
 */
+
+var twoSum = function(nums, target) {
+  var resultArr = [];
+    for(var i = 0; i < nums.length; i++) {
+      for(var j = i+1; j < nums.length; j ++)   {
+        if(nums[i] + nums[j] === target) {
+          return resultArr = [i, j];
+        }
+      }      
+    }
+};
